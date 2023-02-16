166737eab8f8c74c1ee931a4d77f1ab199686c2d
Cheikh Ndiaye <cheikhn414@gmail.com>
2023-02-16 14:44:41 +0000

    Spring Boot project init with require deps

---
+++ src/main/java/com/atos/devops/devOpsProjetCv/DevOpsProjetCvApplication.java
@@ -0,0 +1,13 @@
+package com.atos.devops.devOpsProjetCv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsProjetCvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsProjetCvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atos/devops/devOpsProjetCv/DevOpsProjetCvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.devops.devOpsProjetCv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsProjetCvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
